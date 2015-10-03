511d59d00db6bf923f616b5ac8d7aac19490eb72
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2015-10-03 14:57:04 +0100

    Change a couple of default paths.

--- jsclient/client.js
+++ jsclient/client.js
@@ -222,7 +222,7 @@ init_search = make_promise(_init_search);
 function play_audio() {
   // change src to bypass caching
   var p = Math.floor(Math.random() * 10000);
-  speaker.src = "http://" + window.location.hostname + ":9091/stream.ogg?" + p;
+  speaker.src = "http://" + window.location.hostname + "/stream?" + p;
   speaker.play();
 }
 
